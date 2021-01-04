da17317ac21eed154bcebfe365af00d45ad8b4ad
Romain Tello <romain.tello@gmail.com>
2021-01-04 11:13:43 +0100

    Add static todo list

--- src/App.js
+++ src/App.js
@@ -3,21 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ma liste de courses</h1>
+      <ul>
+        <li>
+          <h3>Bananes</h3>
+        </li>
+        <li>
+          <h3>Poires</h3>
+        </li>
+        <li>
+          <h3>Chocolat</h3>
+        </li>
+      </ul>
     </div>
   );
 }
